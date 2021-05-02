fe2e2a166cd77d49ce1550897cee21f864d85fe3
junio salome <junio@gbctech.com.br>
2021-05-02 00:38:31 -0300

    vue

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    <Leaderboard />
+  </div>
+</template>
+
+<script>
+import Leaderboard from "./components/Leaderboard";
+export default {
+  name: "App",
+  components: {
+    Leaderboard,
+  },
+};
+</script>
