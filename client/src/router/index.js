7af205c05698e2bba38741a50b5e10e4961ff3c9
junio salome <junio@gbctech.com.br>
2021-05-03 05:00:23 -0300

    casca para front

---
+++ client/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../components/Ping.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    },
+  ],
+});
